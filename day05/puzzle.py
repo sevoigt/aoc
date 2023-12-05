758c56a854482e6013e7d89d02604a75ab174988
Sebastian Voigt <svoigt>
2023-12-05 23:16:05 +0100

    attempt for part 2 - seems correct but waaaay to slow

--- day05/puzzle.py
+++ day05/puzzle.py
@@ -1,7 +1,7 @@
 """
 day 5
 
-numbers are too large to use a dict -> out of memory
+numbers are too large to use a mapping dict -> out of memory
 """
 
 
@@ -19,7 +19,7 @@ def within(value, min_val, max_val):
 
 def convert(block, value):
     """
-    yeah its inefficient...
+    yeah its inefficient...but ok for part 1
     """
     lines = block.split('\n')
     
@@ -31,14 +31,44 @@ def convert(block, value):
     
     return value
 
+
+def preprocess_block(block):
+    """
+    Preprocessing / caching to avoid parsing the block for each seed
+    block is described by start, end and offset between src and dest
+    """
+    res = list()
+
+    lines = block.split('\n')
     
+    for line in lines[1:]:
+        dest, src, num = get_numbers(line)
+        res.append((src, src+num-1, dest-src))
+
+    return res
+    
+
+def convert_pp(block, value):
+    """
+    Use cached block data
+    """
+    for line in block:
+        if within(value, line[0], line[1]):
+            value += line[2]
+            break
+    
+    return value
+
 
 fid = open("input.txt")
 c = fid.read()
 
 blocks = c.split('\n\n')
 
+
+# part 1
 seeds = get_numbers(blocks[0])
+
 locations = []
 
 for i in seeds:
@@ -48,3 +78,21 @@ for i in seeds:
     locations.append(i)
 
 print("result part 1:", min(locations))
+
+
+# part 2
+ranges = list(zip(seeds[:-1], seeds[1:]))[::2]
+block_data = [preprocess_block(i) for i in blocks[1:]]
+print('ranges', len(ranges))
+
+smallest = 1e12
+
+for i in ranges:
+    print('processing range', i)
+    for j in range(i[0], i[0]+i[1]):
+        for k in block_data:
+            j = convert_pp(k, j)
+        
+        smallest = min(j, smallest)
+
+print("result part 2:", smallest)
