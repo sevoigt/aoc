a018f736314583c69cdb4d9de39cad10e4fe7023
Sebastian Voigt <svoigt>
2023-12-09 01:31:29 +0100

    linked objects, still too slow

--- day08/puzzle.py
+++ day08/puzzle.py
@@ -2,6 +2,19 @@
 day 8
 """
 
+
+class Node(object):
+    
+    def __init__(self, name):
+        self.name = name
+        self.finish = name.endswith('Z')
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return f'<Node {self.name}>'
+
+
 fid = open('input.txt')
 lines = fid.readlines()
 
@@ -33,17 +46,28 @@ print('result part 1:', steps)
 
 # part 2
 
-next_nodes = [i for i in nodes.keys() if i.endswith('A')]
 all_z = False
 steps = 0
 directions = [i == 'L' for i in directions]
 
+# now every node has a link to its next left/right node
+nodeobjs = dict()
+for i in nodes.keys():
+    nodeobjs.update({i : Node(i)})
+
+for i in nodes.keys():
+    nodeobjs[i].left = nodeobjs[nodes[i][0]]
+    nodeobjs[i].right = nodeobjs[nodes[i][1]]
+
+next_nodes = [nodeobjs[i] for i in nodeobjs.keys() if i.endswith('A')]
+
+
 while not all_z:
     _d, _m = divmod(steps, num_directions)
     left = directions[_m]
-    next_nodes = [nodes[i][0] for i in next_nodes] if left else \
-                 [nodes[i][1] for i in next_nodes] 
-    all_z = sum([0 if i.endswith('Z') else 1 for i in next_nodes]) == 0 
+    next_nodes = [i.left for i in next_nodes] if left else \
+                 [i.right for i in next_nodes] 
+    all_z = sum([0 if i.finish else 1 for i in next_nodes]) == 0 
     steps += 1
     
 print('result part 2:', steps)
