8a33b48b95afb41548345cf77f0e899dbfb26e5f
Sebastian Voigt <svoigt>
2024-12-02 00:13:35 +0100

    reorganized repo into years, solution for 01/2024

---
+++ 2024/day01/puzzle.py
@@ -0,0 +1,26 @@
+"""
+2024/01
+"""
+
+import numpy as np
+
+print("hello")
+
+data = np.loadtxt('input.txt', dtype=np.int64)
+
+l1 = data[:,0]
+l2 = data[:,1]
+
+l1.sort()
+l2.sort()
+
+# part 1
+print(sum(np.abs(l2-l1)))
+
+# part2
+res = 0
+
+for i in l1:
+    res += i*len(l2[l2==i])
+
+print(res)
